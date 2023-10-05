b67edca276b50aeb2ae1dc9c9e64d21e092a9c61
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-04 21:08:20 -0700

    made small changes to the index.js file to speed up v.k. mongodb set up time

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
-const apiPort = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
+
+//ConnectionUrl: the url mongodb atlas give you to connect to db. PORT: is the port the server will run on
+const CONNECTION_URL = '[connection url]'
+const PORT = proccess.env.Port || 5000
+
+//Setting up the connection to mongodb atlas
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch(() => console.log(error))
+
+mongoose.set('useFindAndModify', false)
+
 
 // Basic app get request for info
 app.get('/', (req, res) => {
@@ -18,4 +31,3 @@ app.get('/hotel', (req, res) => {
     res.send('Hotels Information')
 })
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
