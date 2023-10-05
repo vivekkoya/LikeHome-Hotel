88949f77d60b65624f61dc040cef828349667819
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-04 22:36:09 -0700

    connection to mongodb set

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -10,15 +11,15 @@ app.use(cors())
 app.use(bodyParser.json())
 
 //ConnectionUrl: the url mongodb atlas give you to connect to db. PORT: is the port the server will run on
-const CONNECTION_URL = '[connection url]'
-const PORT = proccess.env.Port || 5000
+const CONNECTION_URL = 'mongodb+srv://Winson:yptSeHMywBWNWR4G@hotel.pu5yiij.mongodb.net/?retryWrites=true&w=majority'
+const PORT = 5001
 
 //Setting up the connection to mongodb atlas
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch(() => console.log(error))
+    .catch((error) => console.log(error))
 
-mongoose.set('useFindAndModify', false)
+
 
 
 // Basic app get request for info
