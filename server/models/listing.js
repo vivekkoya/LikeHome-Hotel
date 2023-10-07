20159f7f4209d410f50fc90985c061bf60cf9dd9
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-07 15:51:23 -0700

    added listings backend to server

--- server/models/listing.js
+++ server/models/listing.js
@@ -1,25 +1,58 @@
-import mongoose from 'mongoose';
+import mongoose, { trusted } from 'mongoose';
 
-const room_details = mongoose.Schema({
-    beds: Number,
-    max_people: Number,
-    type: String,
-    bathrooms: Number,
+const roomSchema = mongoose.Schema({
+    beds: {
+        type: Number,
+        required: true
+    },
+    max_people: {
+        type: Number,
+        required: true
+    }, 
+    type: {
+        type: String,
+        required: true
+    },
+    bathrooms: {
+        type: Number,
+        required: true
+    },
 })
 
-const location = mongoose.Schema({
-    state: String,
-    city: String,
-    address: String,
-    zipcode: String
+const locationSchema = mongoose.Schema({
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
 })
 
 const postSchema = mongoose.Schema({
-    hotel_name: String,
-    price: Number,
-    location: location,
-    room_details: room_details,
+    hotel_name: { 
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: locationSchema,
+        required: true
+    },
+    room_details: {
+        type: roomSchema,
+        required: true
+    },
     amenities: [String],
+    accessability: [String],
     check_in: {
         type: String,
         match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, // Regex pattern for validating the time format
@@ -34,5 +67,5 @@ const postSchema = mongoose.Schema({
 
 })
 
-const Listing = model('Listing', postSchema)
-export default Lisitng 
+const Listing = mongoose.model('Listing', postSchema)
+export default Listing 
--- server/routes/listings.js
+++ server/routes/listings.js
@@ -2,8 +2,61 @@ import express from 'express'
 
 const router = express.Router();
 
+import Listing from '../models/listing.js'
+
+//localhost:5000/lisitngs
+
 router.get('/', (req, res) => {
-    res.send('This Works!');
+    try {
+
+    } catch (error) {
+
+    }
 })
+
+
+router.get('/createListing', async (req, res) => {
+    const listing = req.body
+    const newLisitng = new Listing(listing)
+    try {
+        await newLisitng.save();
+
+        res.status(201).json(newLisitng)
+    } catch (error) {
+        res.status(409).json( { message: error.message })
+    }
+})
+
+
+router.get('/getListings', async (req, res) => {
+    try {
+        const postListing = await Listing.find().lean()
+
+        console.log(postListing)
+
+        res.status(200).json(postListing);
+
+    } catch (error) {
+        res.status(404).json({messages: error.message })
+    }
+})
+
+router.delete('/deleteListing/:listingId', async (req ,res) => {
+    const listingId = req.params.listingId;
+    
+    try {
+        console.log(listingId)
+        const deletedListing = await Listing.findByIdAndRemove(listingId)
+
+        if (!deletedListing) {
+            throw new Error(`Listing not found`)
+        }
+        res.status(200).json({message: "Listing succesfully deleted"})
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+})
+
+
 
 export default router
