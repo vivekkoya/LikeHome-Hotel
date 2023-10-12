940fffad386a79c482326ae21002beec2c608983
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-12 00:37:40 -0700

    added a Navbar

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,5 +1,5 @@
-
 import './App.css';
+import NavBar from './components/NavBar';
 import LoginSignup from './loginpage/LoginSignup';
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
@@ -7,15 +7,18 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/login" element={<LoginSignup/>}/>
-        <Route exact path="/signup" element={<h1>Sign Up</h1>}/>
-        <Route exact path="/viewreservations" element={<h1>User Reservations Page</h1>}/>
-        <Route exact path="/hotelviewbookings" element={<h1>Hotel Bookings Page</h1>}/>
-        <Route exact path="/" element={<h1>Search Listing Page</h1>}/>
-      </Routes>
-    </Router>
+    <>
+      <NavBar/>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<LoginSignup/>}/>
+          <Route exact path="/signup" element={<h1>Sign Up</h1>}/>
+          <Route exact path="/viewreservations" element={<h1>User Reservations Page</h1>}/>
+          <Route exact path="/hotelviewbookings" element={<h1>Hotel Bookings Page</h1>}/>
+          <Route exact path="/" element={<h1>Search Listing Page</h1>}/>
+        </Routes>
+      </Router>
+    </>
 )}
 
 export default App;
