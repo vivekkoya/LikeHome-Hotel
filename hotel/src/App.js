e9ca47896d821a87d15c2e7111d73667d3690471
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-11 22:41:43 -0700

    Adding Routing Functionality to the app to support multiple pages- this update pachage.json

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 import LoginSignup from './loginpage/LoginSignup';
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-      <LoginSignup/>
-    </div>
-  );
-}
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<LoginSignup/>}/>
+        <Route exact path="/signup" element={<h1>Sign Up</h1>}/>
+        <Route exact path="/viewreservations" element={<h1>User Reservations Page</h1>}/>
+        <Route exact path="/hotelviewbookings" element={<h1>Hotel Bookings Page</h1>}/>
+        <Route exact path="/" element={<h1>Search Listing Page</h1>}/>
+      </Routes>
+    </Router>
+)}
 
 export default App;
