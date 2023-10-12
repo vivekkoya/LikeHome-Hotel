a87cb4fbda687ddbe8c7cadebdaca61ac520174a
Etienne Devictor <etiennedevictor@Etiennes-MacBook-Air.local>
2023-10-11 21:18:28 -0700

    added an image folder in public and populated it with some images we can use for listings

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import LoginSignup from './loginpage/LoginSignup';
 
+
 function App() {
   return (
     <div className="App">
       <LoginSignup/>
+      <img src="/images/Listng1.jpeg" alt="Image" ></img>
     </div>
   );
 }
