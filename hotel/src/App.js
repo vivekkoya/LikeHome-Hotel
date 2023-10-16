446e22b1a5577a673b5b5b7e265056f60c507bab
ayushshresth021 <ayush.shresth17@gmail.com>
2023-10-15 18:04:13 -0700

    login and signup

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import styled from "styled-components";
 import NavBar from './components/NavBar';
-import LoginSignup from './loginpage/LoginSignup';
+import { AccountBox, accountBox } from "./components/accountBox";
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { LoginForm } from './components/accountBox/loginForm';
 
+
+const AppContainer = styled.div`
+  
+  height: 100%;
+  display: flex;
+  // flex-direction: column;
+  // align-items: center;
+  justify-content: center;
+  
+`;
 
 function App() {
   return (
@@ -11,7 +23,7 @@ function App() {
       <NavBar/>
       <Router>
         <Routes>
-          <Route exact path="/login" element={<LoginSignup/>}/>
+          <Route exact path="/login" element={<AppContainer><AccountBox/></AppContainer>}/>
           <Route exact path="/signup" element={<h1>Sign Up</h1>}/>
           <Route exact path="/viewreservations" element={<h1>User Reservations Page</h1>}/>
           <Route exact path="/hotelviewbookings" element={<h1>Hotel Bookings Page</h1>}/>
