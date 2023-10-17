f8e4905c7be149e5974276975ba05685e2e6aa26
Etienne Devictor <devictoretienne@gmail.com>
2023-10-17 12:36:29 -0700

    added cookies to track active user

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import NavBar from './components/NavBar';
 import Listings from './listingspage/Listings';
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { LoginForm } from './components/accountBox/loginForm';
 import {AccountBox} from "./components/accountBox";
 
@@ -18,17 +18,21 @@ const AppContainer = styled.div`
   
 `;
 
+
 function App() {
+
+
+
   return (
     <>
       <NavBar/>
       <Router>
         <Routes>
-          <Route exact path="/login" element={<AppContainer><AccountBox/></AppContainer>}/>
-          <Route exact path="/signup" element={<h1>Sign Up</h1>}/>
-          <Route exact path="/viewreservations" element={<h1>User Reservations Page</h1>}/>
-          <Route exact path="/hotelviewbookings" element={<h1>Hotel Bookings Page</h1>}/>
-          <Route exact path="/" element={<Listings/>}/>
+          <Route  path="/login" element={<AppContainer><AccountBox/></AppContainer>}/>
+          <Route  path="/signup" element={<h1>Sign Up</h1>}/>
+          <Route  path="/viewreservations" element={<h1>User Reservations Page</h1>}/>
+          <Route  path="/hotelviewbookings" element={<h1>Hotel Bookings Page</h1>}/>
+          <Route  path="/" element={<Listings/>}/>
         </Routes>
       </Router>
     </>
--- hotel/src/listingspage/Listings.jsx
+++ hotel/src/listingspage/Listings.jsx
@@ -4,46 +4,47 @@ import ListingCard from "../listingcard/ListingCard";
 
 const Listings = () => {
   const [listings, setListings] = useState([]);
-  const [location, setLocation] = useState(null);
-  let searchQuery = '';
+  // const [location, setLocation] = useState(null);
+  let searchQuery = "";
 
-  useEffect(() => {
-    (async () => {
-      console.log("start fetch");
-      try {
-        const response = await fetch(
-          "http://localhost:5001/listings/getListings"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setListings(data);
-         console.log(location);
-      } catch (error) {
-        console.error("Fetch error:", error);
+  // useEffect(() => {
+  //   (async () => {
+  //     console.log("start fetch");
+  //     try {
+  //       const response = await fetch(
+  //         "http://localhost:5001/listings/getListings"
+  //       );
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //       const data = await response.json();
+  //       setListings(data);
+  //        console.log(location);
+  //     } catch (error) {
+  //       console.error("Fetch error:", error);
+  //     }
+  //   })();
+  // }, [location]);
+
+  const onSearchClick = async () => {
+    const search = searchQuery.replace(" ", "%20");
+    try {
+      const res = await fetch(
+        `http://localhost:5001/listings/ListingInCity/${searchQuery}`
+      );
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
       }
-    })();
-  }, [location]);
-
-    const onSearchClick = async() => {
-        const search = searchQuery.replace(' ', '%20');
-        try {
-        const res = await fetch(`http://localhost:5001/listings/ListingInCity/${searchQuery}`)
-         if (!res.ok) {
-            throw new Error(`HTTP error! Status: ${res.status}`);
-         }
-            const data = await res.json();
-            setListings(data);
-        }
-        catch (error) {
-            console.error("Fetch error:", error);
-        }
+      const data = await res.json();
+      setListings(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
     }
+  };
 
-    const handleChange = (event) => {
-        searchQuery = event.target.value;
-    }
+  const handleChange = (event) => {
+    searchQuery = event.target.value;
+  };
   /* const listings1 = [
     { name: "Marriott", city: "San Jose, CA", rating: "4.7", price: "$150" },
     { name: "Hilton", city: "Santa Clara, CA", rating: "4.3", price: "$175" },
@@ -58,15 +59,24 @@ const Listings = () => {
   ]; */
   return (
     <div className="listings">
-       <div className="header-bar">
+      <div className="header-bar">
         <div className="search-bar">
-              <input className="search-text-field" type="text" placeholder="Where Would You Like To Go?" onChange={handleChange}/>
-            <button className='search-button' type="submit" onClick={onSearchClick}>
-                Search
-            </button>
+          <input
+            className="search-text-field"
+            type="text"
+            placeholder="Where Would You Like To Go?"
+            onChange={handleChange}
+          />
+          <button
+            className="search-button"
+            type="submit"
+            onClick={onSearchClick}
+          >
+            Search
+          </button>
         </div>
-          <h2>Current Listings</h2>
-       </div>
+        <h2>Current Listings</h2>
+      </div>
       <div className="listing-cards">
         {listings.map((listing) => (
           <ListingCard listing={listing} />
