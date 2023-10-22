730522dafe8438deec21f2568b1a3d182f21769f
winson2002 <winsonma2002@gmail.com>
2023-10-22 05:54:56 -0700

    Moved Filter.js to routes and Filter edit02

---
+++ server/routes/Filter.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Listing = require('./listings.js');
+const router = express.Router();
+
+router.get('/hotels', async(req, res) => {
+  try {
+    const{beds, priceMin, priceMax, hotelName, amenities, accessability} = req.query;
+    const filter = {};
+
+    if(beds) {
+      filter['room_details.bed'] = {$gte: parseInt(beds)};
+    }
+
+    if(priceMin || priceMax) {
+      filter.price = {};
+      if(priceMin) {
+        filter.price.$gte = parseInt(priceMin);
+      }
+      if(pricemax) {
+        filter.price.$gte = parseInt(priceMax);
+      }
+    }
+
+    if(hotelName) {
+      filter.hotel_name = new RegExp(hotelName, 'i');
+    }
+
+    if(amenities) {
+      filter.amenities = {$all: amenities.split(',')};
+    }
+
+    if(accessability) {
+      filter.accessability = {$in: accessability.split(',')};
+    }
+
+    const hotels = await Listing.find(filter);
+
+    res.json(hotels);
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({message: 'Server error'});
+  }
+});
+
+module.exports = router;
