c5b77c0f14e899b6627759d33c790c6cd975aeb2
winson2002 <winsonma2002@gmail.com>
2023-11-16 21:34:24 -0800

    Added sorting for beds and people, changed the structure of filtering/sorting, and organized the code in listings.js

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -1,8 +1,7 @@
-import express from 'express'
+import express from 'express';
+import Listing from '../models/listing.js';
 
 const router = express.Router();
-
-import Listing from '../models/listing.js'
 
 //localhost:5001/listngs
 
@@ -14,100 +13,111 @@ router.get('/', (req, res) => {
     }
 })
 
-
-router.get('/createListing', async (req, res) => {
-    const listing = req.body
-    const newLisitng = new Listing(listing)
+// Create a new listing
+router.post('/createListing', async (req, res) => {
     try {
-        await newLisitng.save();
-
-        res.status(201).json(newLisitng)
+        const listingData = req.body;
+        const newListing = new Listing(listingData);
+        await newListing.save();
+        res.status(201).json(newListing)
     } catch (error) {
         res.status(404).json({messages: error.message })
     }
 })
 
-
+// Get listings
 router.get('/getListings/:id', async (req, res) => {
     try {
-        const postListing = await Listing.find({'_id': req.params.id}).lean()
+        const postListing = await Listing.findById({'_id':req.params.id}).lean();
 
-        console.log(postListing)
+        console.log(postListing);
 
         res.status(200).json(postListing);
 
     } catch (error) {
-        res.status(404).json({messages: error.message })
+        res.status(404).json({messages: error.message });
     }
 })
 
+// Delete a listing
 router.delete('/deleteListing/:listingId', async (req ,res) => {
-    const listingId = req.params.listingId;
-    
     try {
-        console.log(listingId)
-        const deletedListing = await Listing.findByIdAndRemove(listingId)
+        const listingId = req.params.listingId;
+
+        console.log(listingId);
+
+        const deletedListing = await Listing.findByIdAndRemove(listingId);
 
         if (!deletedListing) {
-            throw new Error(`Listing not found`)
+            throw new Error(`Listing not found`);
         }
-        res.status(200).json({message: "Listing succesfully deleted"})
+        res.status(200).json({message: "Listing succesfully deleted"});
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.status(404).json({message: error.message});
     }
 })
 
 // Note for multiword cities like san jose use %20 instead of space 
 // ex localhost:5001/listings/ListingInCity/San%20Jose
 // Incorporated filtering hotels
-router.post('/ListingInCity/:city', async (req, res) => {
-    console.log(req.body)
-    console.log(req.params.city);
-    const city = req.params.city
-    const {priceMin, priceMax, beds, people, amenities, accessability, sortBy} = req.body;
-    const query = {'location.city': city};
-    
-    if (priceMin !== "" && !isNaN(priceMin)) {
-        query['price'] = {};
-        query['price']['$gte'] = parseInt(priceMin);
-    }
-    if (priceMax !== "" && !isNaN(priceMax)) {
-        if (!query['price']) {
-            query['price'] = {};
-        }
-        query['price']['$lte'] = parseInt(priceMax);
-    }
-
-    if (beds) {
-        query['room_details.beds'] = parseInt(beds);
-    }
-
-    if (people) {
-        query['room_details.max_people'] = parseInt(people);
-    }
-
-    if (amenities && amenities.length > 0) {
-        query['amenities'] = {$in: amenities};
-    }
-
-    if (accessability && accessability.length > 0) {
-        query['accessability'] = {$in: accessability};
-    }
-
+router.get('/ListingInCity/:city', async (req, res) => {
     try {
-        let sortedListings = Listing.find(query).lean();
-
-        // Sort by price per person or by price alone
-        if (sortBy === "priceAsc") {
-            sortedListings = sortedListings.sort({price: 1});
+        console.log(req.params.city);
+        const city = req.params.city
+        const {priceMin, priceMax, beds, people, amenities, accessability, sortBy} = req.query;
+        const query = {'location.city': city};
+        
+        if (priceMin && !isNaN(priceMin)) {
+            query['price'] = {$gte: parseInt(priceMin)};
         }
-        else if (sortBy === "priceDesc") {
-            sortedListings = sortedListings.sort({price: -1});
+        if (priceMax && !isNaN(priceMax)) {
+            query['price'] = query['price'] || {};
+            query['price']['$lte'] = parseInt(priceMax);
         }
 
-        const filteredListings = await sortedListings.exec();
+        if (beds) {
+            query['room_details.beds'] = parseInt(beds);
+        }
 
-        res.status(200).json(filteredListings);
+        if (people) {
+            query['room_details.max_people'] = parseInt(people);
+        }
+
+        if (amenities) {
+            query['amenities'] = {$all: amenities.split(',').map(amenity => amenity.trim())};
+        }
+
+        if (accessability) {
+            query['accessability'] = {$all: accessability.split(',')};
+        }
+
+        let sortOptions = {};
+        switch (sortBy) {
+            case 'priceAsc':
+                sortOptions = {price: 1};
+                break;
+            case 'priceDesc':
+                sortOptions = {price: -1};
+                break;
+            case 'bedsAsc':
+                sortOptions = {'room_details.beds': 1};
+                break;
+            case 'bedsDesc':
+                sortOptions = {'room_details.beds': -1};
+                break;
+            case 'peopleAsc':
+                sortOptions = {'room_details.max_people': 1};
+                break;
+            case 'peopleDesc':
+                sortOptions = {'room_details.max_people': -1};
+                break;
+            default:
+                sortOptions = {price: 1};
+        }
+
+        const sortedListings = await Listing.find(query).sort(sortOptions).lean();
+
+        res.status(200).json(sortedListings);
     } catch (error) {
         res.status(404).json({message: error.message});
     }
