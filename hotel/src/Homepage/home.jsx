9b90b871bc6d131638ac0c9fc66f502145eb36b6
vivekkoya <13vivekkoya@gmail.com>
2023-11-27 10:16:40 -0800

    Update name

--- hotel/src/Homepage/home.jsx
+++ hotel/src/Homepage/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Reveal from "react-reveal/Reveal";
-import Fade from "react-reveal/Fade";
-import Zoom from "react-reveal/Zoom";
+// import Reveal from "react-reveal/Reveal";
+// import Fade from "react-reveal/Fade";
+// import Zoom from "react-reveal/Zoom";
 class Home extends React.Component {
   render() {
     return (
